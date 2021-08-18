db7030c01acb475f4fd5f9204910d92bc57cfa34
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-08-17 22:29:30 -0400

    Implemented DFT and inverse DFT

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+import sgn.signal as signal
 from sgn.signal import Signal
+from transforms.fourier import Fourier, InverseFourier
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 if __name__ == '__main__':
     wave1 = {
-        "freq": 0.5,
+        "freq": 2 * np.pi * 5,
         "amp": 10
     }
     wave2 = {
-        "freq": 25,
+        "freq": 2 * np.pi * 250,
         "amp": 5
     }
     wave3 = {
-        "freq": 125,
+        "freq": 2 * np.pi * 600,
         "amp": 2.5
     }
 
-    time = np.linspace(0, 100, 100000)
+    time = np.linspace(-1500, 1500, 3000)
 
     signal1 = Signal.from_function(
         time,
@@ -32,10 +35,40 @@ if __name__ == '__main__':
         lambda t: wave3["amp"] * np.sin(wave3["freq"] * t)
     )
 
+    sq_signal = signal.SQUARE(time, 100e-4, 10)
+
+    noise0 = signal.NOISE(time, 0.1)
+    noise1 = signal.NOISE(time, 1)
+    noise2 = signal.NOISE(time, 2)
+    noise3 = signal.NOISE(time, 3)
+    noise4 = signal.NOISE(time, 4)
+    noise5 = signal.NOISE(time, 5)
+
+    noise_m_0 = signal.NOISE(time, 0.1, add=False)
+    noise_m_1 = signal.NOISE(time, 1, add=False)
+    noise_m_2 = signal.NOISE(time, 2, add=False)
+    noise_m_3 = signal.NOISE(time, 3, add=False)
+    noise_m_4 = signal.NOISE(time, 4, add=False)
+    noise_m_5 = signal.NOISE(time, 5, add=False)
+
+    heaviside1 = signal.HEAVISIDE(time, 1000)
+
+    # fourier = Fourier(signal1).calculate()
+    # sum_signal = signal1 + signal2 + signal3
+
     fig, ax = plt.subplots()
-    # plt.plot(*signal1.unpack(), color="r")
+
+    # plt.plot(*signal1.unpack(), color="b")
     # plt.plot(*signal2.unpack(), color="b")
-    # plt.plot(*signal3.unpack(), color="g")
-    # plt.plot(*(signal1 + signal3).rect_smooth(1501).unpack(), color="r")
+    # plt.plot(*signal3.unpack(), color="b")
+    # plt.plot(*sum_signal.unpack(), color="b")
+    # plt.plot(*(sq_signal + noise5).unpack(), color="b")
+
+    fourier = Fourier(sq_signal).calculate()#.half()
+
+    plt.plot(*abs(fourier).unpack(), color="r")
+    plt.plot(*InverseFourier(fourier).calculate().unpack(), color="b")
+
+    # plt.xlim(0)
 
     plt.show()
---
+++ transforms/fourier.py
@@ -0,0 +1,66 @@
+import sgn.signal
+from transforms import Transform
+from config import FOURIER_METHOD
+
+import numpy as np
+import tqdm
+
+
+class Fourier(Transform):
+    """Fourier transform"""
+    def calculate(self):
+        return self._calculate(FOURIER_METHOD)
+
+    def _calculate(self, method):
+        if method == "dft":
+            return self._calculate_dft()
+
+    def _calculate_dft(self):
+        """Calculates the Discrete Fourier Transform (DFT) of a signal :math:`\\mathcal{F}\\{x[n]\\} = X[k]`, such that
+
+        .. math::
+            X[k] = \\sum_{n=0}^{N-1}x[n]e^{-j2\\pi nk/N}
+
+        Returns
+        -------
+        Signal representing the Fourier Transform.
+        """
+        signal_len = len(self.signal)
+        new_values = np.zeros(signal_len, dtype=complex)
+        for k in tqdm.tqdm(range(signal_len), "Calculating DFT..."):
+            temp = 0 + 0j
+            for n in range(signal_len):
+                temp += self.signal.values[n] * np.exp(-1j * (2 * n * k * np.pi / signal_len))
+            new_values[k] = temp
+            self.values = new_values
+        return sgn.signal.Signal(self.time, self.values) / signal_len
+
+
+class InverseFourier(Transform):
+    """Inverse Fourier transform"""
+    def calculate(self):
+        return self._calculate(FOURIER_METHOD)
+
+    def _calculate(self, method):
+        if method == "dft":
+            return self._calculate_dft()
+
+    def _calculate_dft(self):
+        """Calculates the inverse Fourier Transform :math:`\\mathcal{F}^{-1}\\{X[k]\\} = x[n]` such that
+
+         .. math::
+            x[n] = \\sum_{k=0}^{N-1}X[k]e^{j2\\pi nk/N}
+
+        Returns
+        -------
+        Signal representing the Inverse Fourier Transform.
+        """
+        signal_len = len(self.signal)
+        new_values = np.zeros(signal_len, dtype=complex)
+        for n in tqdm.tqdm(range(signal_len), "Calculating Inverse DFT..."):
+            temp = 0 + 0j
+            for k in range(signal_len):
+                temp += self.signal.values[k] * np.exp(1j * (2 * n * k * np.pi / signal_len))
+            new_values[n] = temp / signal_len
+            self.values = new_values
+        return sgn.signal.Signal(self.time, self.values) * signal_len
