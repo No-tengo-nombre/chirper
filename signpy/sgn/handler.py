cb61456fa248a45a6d5212df2b32bd8c1a85d87d
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-02 04:42:56 -0300

    Update handler.py

--- signpy/sgn/handler.py
+++ signpy/sgn/handler.py
@@ -6,17 +6,17 @@ import csv
 class HandlerCSV:
     """Class for handling imports and exports with .csv files."""
     @staticmethod
-    def export_signal1(filename: str, time_signal: signal.TimeSignal):
+    def export_signal1(filename: str, signal1: signal.Signal1):
         """Exports the given one dimensional signal to the .csv file."""
         with open(filename, "w+") as file:
             writer = csv.writer(file)
-            writer.writerows(*time_signal.unpack())
+            writer.writerows(*signal1.unpack())
 
 
 class HandlerJSON:
     """Class for handling imports and exports with .json files."""
     @staticmethod
-    def export_signal1(filename: str, time_signal: signal.TimeSignal):
+    def export_signal1(filename: str, signal1: signal.Signal1):
         """Exports the given one dimensional signal to the .json file."""
         with open(filename, "w+") as file:
             pass
