fb412805d67e73c6258d4201ec911b1857f9a9c3
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-20 20:18:21 -0300

    Update run_manual_tests.py

--- test/run_manual_tests.py
+++ test/run_manual_tests.py
@@ -5,15 +5,17 @@ from . import (manual_am_modulation, manual_conv, manual_io,
     manual_hilbert, manual_fourier, manual_modulation)
 
 
+SHOW_ALL_FIGS = False
+
 tests = (
-    manual_am_modulation,
-    manual_conv,
-    manual_io,
-    manual_hilbert,
-    manual_fourier,
-    manual_modulation,
+    (manual_am_modulation, False),
+    (manual_conv, False),
+    (manual_io, False),
+    (manual_hilbert, False),
+    (manual_fourier, False),
+    (manual_modulation, False),
 )
 
-for test in tqdm(tests, desc="Running manual tests"):
-    test.main(False)
-# plt.show()
+for test, show in tqdm(tests, desc="Running manual tests"):
+    test.main(show)
+if SHOW_ALL_FIGS: plt.show()
