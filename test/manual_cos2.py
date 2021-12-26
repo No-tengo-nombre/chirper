6ee9605db1d89ec059a398a6311e7145d6527cb6
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-26 03:44:16 -0300

    Changed tests

--- test/manual_cos2.py
+++ test/manual_cos2.py
@@ -9,27 +9,38 @@ def f(x, y):
     return np.sin(x ** 2 + y ** 2)
     # return np.sin(1.1 * x + 0.6 * y)
 
+def main(show_fig=False):
+    axis = np.linspace(0, 20, 50)
 
-axis = np.linspace(0, 20, 50)
+    Y = Signal2.from_function(axis, axis, f)
 
-Y = Signal2.from_function(axis, axis, f)
+    fig, ax = plt.subplots()
+    fig.suptitle("Original")
+    plt.imshow(Y.values, cmap="gray", origin="lower")
+    # plt.contourf(*Y.unpack(), cmap="Greys")
 
-fig, ax = plt.subplots()
-fig.suptitle("Original")
-plt.imshow(Y.values, cmap="gray", origin="lower")
-# plt.contourf(*Y.unpack(), cmap="Greys")
+    y_c2 = cosine.c2(Y, 2)
+    y_c4 = cosine.c2(Y, 4)
 
-y_c2 = cosine.c2(Y, 2)
-y_c4 = cosine.c2(Y, 4)
+    fig, ax = plt.subplots()
+    fig.suptitle("Cosine 2")
+    plt.imshow(y_c2.values, cmap="gray", origin="lower")
+    # plt.contourf(*y_c2.unpack(), cmap="gray")
 
-fig, ax = plt.subplots()
-fig.suptitle("Cosine 2")
-plt.imshow(y_c2.values, cmap="gray", origin="lower")
-# plt.contourf(*y_c2.unpack(), cmap="gray")
+    fig, ax = plt.subplots()
+    fig.suptitle("Cosine 4")
+    plt.imshow(y_c4.values, cmap="gray", origin="lower")
+    # plt.contourf(*y_c4.unpack(), cmap="gray")
 
-fig, ax = plt.subplots()
-fig.suptitle("Cosine 4")
-plt.imshow(y_c4.values, cmap="gray", origin="lower")
-# plt.contourf(*y_c4.unpack(), cmap="gray")
+    ################################################################################################################
+    ################################################################################################################
+    ################################################################################################################
 
-plt.show()
+    if show_fig:
+        plt.show()
+    else:
+        plt.close("all")
+
+
+if __name__ == "__main__":
+    main(True)
--- test/run_manual_tests.py
+++ test/run_manual_tests.py
@@ -1,24 +1,26 @@
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-from test import manual_interp1
-
 from . import *
+
 
 SHOW_ALL_FIGS = False
 
 # All these tests are imported with the `from . import *` line
 tests = (
-    (manual_interp1, False),
-    (manual_conv1, False),
-    (manual_conv2, False),
-    (manual_io1, False, False),
-    (manual_io2, False, False),
-    (manual_hilbert, False),
-    (manual_fourier1, False),
-    (manual_fourier2, False),
-    (manual_modulation, False),
-    (manual_am_modulation, False),
+    # (manual_interp1, False),
+    # (manual_conv1, False),
+    # (manual_conv2, False),
+    # (manual_io1, False, False),
+    (manual_io2, True, False),
+    # (manual_hilbert, False),
+    # (manual_cos1, False),
+    # (manual_cos2, False),
+    # (manual_sin1, False),
+    # (manual_fourier1, False),
+    # (manual_fourier2, False),
+    # (manual_modulation, False),
+    # (manual_am_modulation, False),
 )
 
 for test, *control in tqdm(tests, desc="Running manual tests"):
