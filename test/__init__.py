b780bd08fa345869cb5f78e833b5374c7cb53947
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-25 21:08:36 -0300

    Tidied tests implementation

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["manual_am_modulation", "manual_conv1", "manual_conv2",
+           "manual_hilbert", "manual_fourier1", "manual_fourier2",
+           "manual_io1", "manual_io2", "manual_modulation",
+           "test_signal"]
--- test/manual_io2.py
+++ test/manual_io2.py
@@ -5,54 +5,67 @@ from signpy import kernel
 from signpy.sgn import Signal2
 
 
-signal = Signal2.from_file("test/img/cat.png")
-signal_r = Signal2.from_file("test/img/cat.png", "r")
-signal_g = Signal2.from_file("test/img/cat.png", "g")
-signal_b = Signal2.from_file("test/img/cat.png", "b")
+def main(show_fig=False, export=True):
+    signal = Signal2.from_file("test/img/cat.png")
+    signal_r = Signal2.from_file("test/img/cat.png", "r")
+    signal_g = Signal2.from_file("test/img/cat.png", "g")
+    signal_b = Signal2.from_file("test/img/cat.png", "b")
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-fig.suptitle("Original")
-ax1.set_title("Mean channel")
-ax2.set_title("RGB channel")
-im1 = ax1.imshow(signal.values, cmap="gray")
-im2 = ax2.imshow(np.stack([signal_r.values, signal_g.values, signal_b.values], axis=2))
-plt.colorbar(im1, ax=ax1)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle("Original")
+    ax1.set_title("Mean channel")
+    ax2.set_title("RGB channel")
+    im1 = ax1.imshow(signal.values, cmap="gray")
+    im2 = ax2.imshow(
+        np.stack([signal_r.values, signal_g.values, signal_b.values], axis=2))
+    plt.colorbar(im1, ax=ax1)
 
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
-fig.suptitle("Original")
-ax1.set_title("Mean channel")
-ax2.set_title("Red channel")
-ax3.set_title("Green channel")
-ax4.set_title("Blue channel")
-im1 = ax1.imshow(signal.values, cmap="gray")
-im2 = ax2.imshow(signal_r.values, cmap="Reds")
-im3 = ax3.imshow(signal_g.values, cmap="Greens")
-im4 = ax4.imshow(signal_b.values, cmap="Blues")
+    fig.suptitle("Original")
+    ax1.set_title("Mean channel")
+    ax2.set_title("Red channel")
+    ax3.set_title("Green channel")
+    ax4.set_title("Blue channel")
+    im1 = ax1.imshow(signal.values, cmap="gray")
+    im2 = ax2.imshow(signal_r.values, cmap="Reds")
+    im3 = ax3.imshow(signal_g.values, cmap="Greens")
+    im4 = ax4.imshow(signal_b.values, cmap="Blues")
 
-plt.colorbar(im1, ax=ax1)
-plt.colorbar(im2, ax=ax2)
-plt.colorbar(im3, ax=ax3)
-plt.colorbar(im4, ax=ax4)
+    plt.colorbar(im1, ax=ax1)
+    plt.colorbar(im2, ax=ax2)
+    plt.colorbar(im3, ax=ax3)
+    plt.colorbar(im4, ax=ax4)
 
-signal = Signal2.from_freq(signal[100:200, 100:200])
-signal_mean = signal.convolute(kernel.ker_mean(3))
-signal_edge = signal.convolute(kernel.ker_edge())
-signal_sharpen = signal.convolute(kernel.ker_sharpen())
+    signal = Signal2.from_freq(signal[100:200, 100:200])
+    signal_mean = signal.convolute(kernel.ker_mean(3))
+    signal_edge = signal.convolute(kernel.ker_edge())
+    signal_sharpen = signal.convolute(kernel.ker_sharpen())
 
-fig, ax = plt.subplots()
-fig.suptitle("Mean 3x3")
-plt.imshow(signal_mean.values, cmap="gray")
-plt.colorbar()
+    fig, ax = plt.subplots()
+    fig.suptitle("Mean 3x3")
+    plt.imshow(signal_mean.values, cmap="gray")
+    plt.colorbar()
 
-fig, ax = plt.subplots()
-fig.suptitle("Edge")
-plt.imshow(signal_edge[1:-1, 1:-1], cmap="gray")
-plt.colorbar()
+    fig, ax = plt.subplots()
+    fig.suptitle("Edge")
+    plt.imshow(signal_edge[1:-1, 1:-1], cmap="gray")
+    plt.colorbar()
 
-fig, ax = plt.subplots()
-fig.suptitle("Sharpen")
-plt.imshow(signal_sharpen.values, cmap="gray")
-plt.colorbar()
+    fig, ax = plt.subplots()
+    fig.suptitle("Sharpen")
+    plt.imshow(signal_sharpen.values, cmap="gray")
+    plt.colorbar()
 
-plt.show()
+    ################################################################################################################
+    ################################################################################################################
+    ################################################################################################################
+
+    if show_fig:
+        plt.show()
+    else:
+        plt.close("all")
+
+
+if __name__ == "__main__":
+    main(True)
