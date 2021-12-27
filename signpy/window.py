c7ffc99ecc319b813d5805ac80455999e66f196d
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-26 22:58:19 -0300

    Update window.py

--- signpy/window.py
+++ signpy/window.py
@@ -5,13 +5,14 @@ from signpy.sgn import Signal1
 
 def w_rectangular(samp_time):
     epsilon = 0.01
-    axis = [-samp_time / 2 - epsilon, -samp_time / 2, samp_time + epsilon]
+    axis = [-samp_time / 2 - epsilon, -samp_time / 2, samp_time / 2, samp_time / 2 + epsilon]
     values = [0, 1, 1, 0]
     return Signal1(axis, values)
 
 
 def w_gaussian(samp_time, resolution=100):
     axis = np.linspace(-samp_time / 2, samp_time / 2, resolution)
-    values = np.exp(-(axis ** 2) / (2 * samp_time ** 2)) / \
-        np.sqrt(2 * np.pi * samp_time ** 2)
+    sigma = 3 * samp_time
+    values = np.exp(-(axis ** 2) / (2 * sigma ** 2)) / \
+        np.sqrt(2 * np.pi * sigma ** 2)
     return Signal1(axis, values)
