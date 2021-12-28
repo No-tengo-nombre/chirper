702b93172092c605bf5ee535cec863dc76c36da0
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-28 04:08:20 -0300

    Fixed tests

--- test/run_manual_tests.py
+++ test/run_manual_tests.py
@@ -8,20 +8,20 @@ SHOW_ALL_FIGS = False
 
 # All these tests are imported with the `from . import *` line
 tests = (
-    # (manual_interp1, False),
-    # (manual_conv1, False),
-    # (manual_conv2, False),
-    # (manual_io1, False, False),
-    # (manual_io2, False, False),
-    # (manual_hilbert, False),
-    # (manual_cos1, False),
-    # (manual_cos2, False),
-    # (manual_sin1, False),
-    # (manual_fourier1, False),
-    # (manual_fourier2, False),
-    (manual_spectr, True),
-    # (manual_modulation, False),
-    # (manual_am_modulation, False),
+    (manual_interp1, False),
+    (manual_conv1, False),
+    (manual_conv2, False),
+    (manual_io1, False, False),
+    (manual_io2, False, False),
+    (manual_hilbert, False),
+    (manual_cos1, False),
+    (manual_cos2, False),
+    (manual_sin1, False),
+    (manual_fourier1, False),
+    (manual_fourier2, False),
+    (manual_spectr, False),
+    (manual_modulation, False),
+    (manual_am_modulation, False),
 )
 
 for test, *control in tqdm(tests, desc="Running manual tests"):
