8699163c310bbb98ac3f268bb6cf4fc4bd582336
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-07 19:38:59 -0300

    Refactored the sgn subpackage

---
+++ src/chirper/sgn/defaults/__init__.py
@@ -0,0 +1,14 @@
+"""
+Subpackage that provides shortcuts to create some recurring signals,
+such as sinusoids, square signals, etc.
+"""
+
+from .default1 import (
+    SQUARE,
+    SIN,
+    COS,
+    NOISE,
+    HEAVISIDE,
+    IMPULSE,
+    CONSTANT,
+)
