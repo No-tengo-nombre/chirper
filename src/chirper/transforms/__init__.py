0227c13c0ed3d51e946b0d51c098d8577d3f7901
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-07 19:48:57 -0300

    Imported main transform's functions from the directory

--- src/chirper/transforms/__init__.py
+++ src/chirper/transforms/__init__.py
@@ -9,3 +9,10 @@ signal processing purposes. The ones currently available are:
 - Inverse Fourier
 - Hilbert
 """
+
+from .cosine import c1, c2
+from .fourier import f1, f2
+from .hilbert import h1
+from .ifourier import if1, if2
+from .sine import s1, s2
+from .stft import stft1
